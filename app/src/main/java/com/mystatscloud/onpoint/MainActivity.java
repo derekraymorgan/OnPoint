bb4fefa864d64b04c73ebef47ef09baef8da08bc
derekraymorgan <derek.ray.morgan@gmail.com>
2015-11-30 20:45:06 -0800

    Initial commit

---
+++ app/src/main/java/com/mystatscloud/onpoint/MainActivity.java
@@ -0,0 +1,59 @@
+package com.mystatscloud.onpoint;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.Handler;
+
+public class MainActivity extends ActionBarActivity
+{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+
+		// load the splash screen
+		setContentView(R.layout.splash);
+
+		int secondsDelayed = 4;
+
+		// create a new runnable so that the splash screen is not included in the 'back button'
+		new Handler().postDelayed(new Runnable()
+		{
+			public void run()
+			{
+				startActivity(new Intent(MainActivity.this, MainMenuActivity.class));
+				finish();
+			}
+
+		}, secondsDelayed * 1000);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings)
+		{
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+}
