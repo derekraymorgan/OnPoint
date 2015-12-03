1fcc00812e27e897a75a434e276919b0301f486f
derekraymorgan <derek.ray.morgan@gmail.com>
2015-12-02 18:54:08 -0800

    Created a sqlLite database which initializes only once when the phone app is installed. Populated the database which practice test tables. Created a a class to access this database.

--- app/src/main/java/com/mystatscloud/onpoint/PracticeExamActivity.java
+++ app/src/main/java/com/mystatscloud/onpoint/PracticeExamActivity.java
@@ -2,13 +2,37 @@ package com.mystatscloud.onpoint;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class PracticeExamActivity extends ActionBarActivity {
+import java.util.List;
+
+public class PracticeExamActivity extends ActionBarActivity
+{
+
+	private ListView listView;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_practice_exam);
+
+		this.listView = (ListView) findViewById(R.id.examListView);
+
+		DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+
+		databaseAccess.open();
+
+		List<String> quotes = databaseAccess.getTestQuestions();
+
+		databaseAccess.close();
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, quotes);
+
+		this.listView.setAdapter(adapter);
+
 	}
 
 }
