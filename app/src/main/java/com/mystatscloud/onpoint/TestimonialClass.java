98512c91056d3910113008e3c3eaa7ce155a2174
jwally <john.deforrest.wally@gmail.com>
2015-12-08 23:35:17 -0800

    Added comments to files.

--- app/src/main/java/com/mystatscloud/onpoint/TestimonialClass.java
+++ app/src/main/java/com/mystatscloud/onpoint/TestimonialClass.java
@@ -1,12 +1,14 @@
 package com.mystatscloud.onpoint;
 
+// The Testimonial class is used to hold the description and details of a testimonial
 public class TestimonialClass {
 
-    private String description;
-    private String author;
-    private String date;
-    private int imgResource;
+    private String description; // What the testimonial testifies
+    private String author; // The author of the testimony
+    private String date; // The date of the testimony
+    private int imgResource; // An image resource to reflect rating
 
+    // Constructor
     public TestimonialClass(String description, String author, String date, int imgResource) {
         this.setDescription(description);
         this.setAuthor(author);
@@ -14,10 +16,10 @@ public class TestimonialClass {
         this.setImgResource(imgResource);
     }
 
+    // Getters and Setters
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
@@ -25,7 +27,6 @@ public class TestimonialClass {
     public String getAuthor() {
         return author;
     }
-
     public void setAuthor(String author) {
         this.author = author;
     }
@@ -33,15 +34,11 @@ public class TestimonialClass {
     public String getDate() {
         return date;
     }
-
     public void setDate(String date) {
         this.date = date;
     }
 
-    public int getImgResource() {
-        return imgResource;
-    }
-
+    public int getImgResource() { return imgResource; }
     public void setImgResource(int imgResource) {
         this.imgResource = imgResource;
     }
--- app/src/main/java/com/mystatscloud/onpoint/TestimonialsActivity.java
+++ app/src/main/java/com/mystatscloud/onpoint/TestimonialsActivity.java
@@ -4,26 +4,33 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.ListView;
 
+// This class holds all the information and logic for the Testimonial activity
 public class TestimonialsActivity extends ActionBarActivity {
 
-	ListView testimonialView;
+	ListView testimonialView;  // the main view for the activity
 
+    // Override the onCreate interface method
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_testimonials);
 
-		testimonialView = (ListView) findViewById(R.id.testimonial_layout);
-        TestimonialsAdapter adapter = new TestimonialsAdapter(getApplicationContext(),R.layout.fragment_testimonials_row);
+        super.onCreate(savedInstanceState); // make sure onCreate is called for parent class
 
-        int i;
-        int testimonialCount = 10;
-
-        String tempDescription = "This is a temp description that will be loaded dynamically";
-        String tempAuthor = "Group 17";
+        // temporary place holder testimonial
+        String tempDescription = "This is a temporary testimonial about the On Point app.";
+        String tempAuthor = "-Group 17";
         String tempDate = "12/1/15";
         int tempImgResource = R.drawable.gold;
 
+        setContentView(R.layout.activity_testimonials);  // set the xml file being used to draw the activity
+
+		testimonialView = (ListView) findViewById(R.id.testimonial_layout);  // fetch the list view in the xml file to operate on
+
+        // create and adapter to move data to the list view
+        TestimonialsAdapter adapter = new TestimonialsAdapter(getApplicationContext(),R.layout.fragment_testimonials_row);
+
+        // populate the data adapter with the testimonials
+        int i;
+        int testimonialCount = 10;
         for( i = 0; i < testimonialCount; i++){
 
             TestimonialClass obj = new TestimonialClass(tempDescription,tempAuthor,tempDate,tempImgResource);
@@ -31,10 +38,7 @@ public class TestimonialsActivity extends ActionBarActivity {
 
         }
 
+        // link the data adapter to the list view for rendering
         testimonialView.setAdapter(adapter);
-
-
-
     }
-
 }
