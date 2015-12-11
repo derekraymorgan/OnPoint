d65a3125211e094119576efccae07341050d9b3c
jennifertang <jennyt2468@yahoo.com>
2015-12-10 16:32:09 -0800

    Comments

--- app/src/main/java/com/mystatscloud/onpoint/ExpandListAdapter.java
+++ app/src/main/java/com/mystatscloud/onpoint/ExpandListAdapter.java
@@ -21,11 +21,21 @@ public class ExpandListAdapter extends BaseExpandableListAdapter {
     private Context context;
     private ArrayList<ExpandListParent> groups;
 
+    /**
+     * Constructor
+     * @param context
+     * @param groups
+     */
     public ExpandListAdapter(Context context, ArrayList<ExpandListParent> groups) {
         this.context = context;
         this.groups = groups;
     }
 
+    /**
+     * Add an item to the parent group
+     * @param item item to be added
+     * @param group group that item is added to
+     */
     public void addItem(ExpandListChild item, ExpandListParent group) {
         if (!groups.contains(group)) {
             groups.add(group);
@@ -35,15 +45,43 @@ public class ExpandListAdapter extends BaseExpandableListAdapter {
         ch.add(item);
         groups.get(index).setItems(ch);
     }
+
+    /**
+     * Gets the data associated with the given child within the given group.
+     * @param groupPosition the position of the group that the child resides in
+     * @param childPosition the position of the child with respect to other children in the group
+     * @return the data of the child
+     */
     public Object getChild(int groupPosition, int childPosition) {
         ArrayList<ExpandListChild> chList = groups.get(groupPosition).getItems();
         return chList.get(childPosition);
     }
 
+    /**
+     * Gets the ID for the given child within the given group. This ID must be unique across all
+     * children within the group. The combined ID (see getCombinedChildId(long, long)) must be
+     * unique across ALL items (groups and all children).
+     * @param groupPosition the position of the group that contains the child
+     * @param childPosition the position of the child within the group for which the ID is wanted
+     * @return the ID associated with the child
+     */
     public long getChildId(int groupPosition, int childPosition) {
         return childPosition;
     }
 
+    /**
+     * Gets a View that displays the data for the given child within the given group.
+     * @param groupPosition the position of the group that contains the child
+     * @param childPosition the position of the child (for which the View is returned) within the group
+     * @param isLastChild Whether the child is the last child within the group
+     * @param view the old view to reuse, if possible. You should check that this view is non-null
+     *                and of an appropriate type before using. If it is not possible to convert this
+     *                view to display the correct data, this method can create a new view. It is not
+     *                guaranteed that the convertView will have been previously created by
+     *                getChildView(int, int, boolean, View, ViewGroup).
+     * @param parent the parent that this view will eventually be attached to
+     * @return the View corresponding to the child at the specified position
+     */
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View view,
                              ViewGroup parent) {
         ExpandListChild child = (ExpandListChild) getChild(groupPosition, childPosition);
@@ -57,6 +95,11 @@ public class ExpandListAdapter extends BaseExpandableListAdapter {
         return view;
     }
 
+    /**
+     * Gets the number of children in a specified group.
+     * @param groupPosition the position of the group for which the children count should be returned
+     * @return the children count in the specified group
+     */
     public int getChildrenCount(int groupPosition) {
         ArrayList<ExpandListChild> chList = groups.get(groupPosition).getItems();
 
@@ -64,18 +107,48 @@ public class ExpandListAdapter extends BaseExpandableListAdapter {
 
     }
 
+    /**
+     * Gets the data associated with the given group.
+     * @param groupPosition the position of the group
+     * @return the data child for the specified group
+     */
     public Object getGroup(int groupPosition) {
         return groups.get(groupPosition);
     }
 
+    /**
+     * Gets the number of groups.
+     * Default method in BaseExpandableListAdapter
+     * @return the number of groups
+     */
     public int getGroupCount() {
         return groups.size();
     }
 
+    /**
+     * Gets the ID for the group at the given position. This group ID must be unique across groups.
+     * The combined ID (see getCombinedGroupId(long)) must be unique across ALL items (groups and all children).
+     * Default method in BaseExpandableListAdapter
+     * @param groupPosition the position of the group for which the ID is wanted
+     * @return the ID associated with the group
+     */
     public long getGroupId(int groupPosition) {
         return groupPosition;
     }
 
+    /**
+     * Gets a View that displays the given group. This View is only for the group--the Views for the
+     * group's children will be fetched using getChildView(int, int, boolean, View, ViewGroup)
+     * @param groupPosition the position of the group for which the View is returned
+     * @param isLastChild is this the last child?
+     * @param view the old view to reuse, if possible. You should check that this view is non-null
+     *             and of an appropriate type before using. If it is not possible to convert this
+     *             view to display the correct data, this method can create a new view. It is not
+     *             guaranteed that the convertView will have been previously created by
+     *             getGroupView(int, boolean, View, ViewGroup).
+     * @param parent the parent that this view will eventually be attached to
+     * @return the View corresponding to the group at the specified position
+     */
     public View getGroupView(int groupPosition, boolean isLastChild, View view,
                              ViewGroup parent) {
         ExpandListParent group = (ExpandListParent) getGroup(groupPosition);
@@ -88,10 +161,22 @@ public class ExpandListAdapter extends BaseExpandableListAdapter {
         return view;
     }
 
+    /**
+     * Indicates whether the child and group IDs are stable across changes to the underlying data.
+     * Default method in BaseExpandableListAdapter
+     * @return boolean whether or not the same ID always refers to the same object
+     */
     public boolean hasStableIds() {
         return true;
     }
 
+    /**
+     * Whether the child at the specified position is selectable
+     * Default method in BaseExpandableListAdapter
+     * @param arg0 the position of the group that contains the child
+     * @param arg1 the position of the child within the group
+     * @return boolean whether the child is selectable.
+     */
     public boolean isChildSelectable(int arg0, int arg1) {
         return true;
     }
--- app/src/main/java/com/mystatscloud/onpoint/expandListView/Classes/ExpandListChild.java
+++ app/src/main/java/com/mystatscloud/onpoint/expandListView/Classes/ExpandListChild.java
@@ -1,29 +1,54 @@
 package com.mystatscloud.onpoint.expandListView.Classes;
 
+/**
+ * Implementation of items in a expandable list
+ */
 public class ExpandListChild
 {
     private String Name;
     private String Tag;
 
+
+    /**
+     * get method for name or value of the item
+     * @return Name
+     */
     public String getName()
     {
         return Name;
     }
 
+    /**
+     * set method for a name or value of the item
+     * @param Name
+     */
     public void setName(String Name) {
         this.Name = unescape(Name);
     }
 
+    /**
+     * get method for item tag
+     * @return Tag
+     */
     public String getTag()
     {
         return Tag;
     }
 
+    /**
+     * set method for item tag
+     * @param Tag
+     */
     public void setTag(String Tag)
     {
         this.Tag = Tag;
     }
 
+    /**
+     * Unescape a string that escaped in database
+     * @param s input string
+     * @return string as unescape, string escapes when inserted in a database
+     */
     private String unescape(String s)
     {
         return s.replaceAll("\\\\n", "\\\n");
