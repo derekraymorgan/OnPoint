953bad3a2a500d3fa66c69a97b03d869c62d9382
derekraymorgan <derek.ray.morgan@gmail.com>
2016-04-06 13:17:59 -0700

    Made skill/category menu functionally. User must know pick at least one test menu option as well. Options are reset upon pressing the back button.

--- app/src/main/java/com/mystatscloud/onpoint/CategoryTestMenuActivity.java
+++ app/src/main/java/com/mystatscloud/onpoint/CategoryTestMenuActivity.java
@@ -1,10 +1,15 @@
 package com.mystatscloud.onpoint;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.SparseBooleanArray;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,10 +17,8 @@ import java.util.List;
  */
 public class CategoryTestMenuActivity extends ActionBarActivity
 {
-	/**
-	 * When this activity is created combine it with the appropriate view
-	 * @param savedInstanceState
-	 */
+	public ArrayList<String> selectedCategories = new ArrayList<String>();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -38,10 +41,59 @@ public class CategoryTestMenuActivity extends ActionBarActivity
 		databaseAccess.close();
 
 		// load the enu items from the list of test categories
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, testCategories);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, testCategories);
 
 		listView.setAdapter(adapter);
 
+	}
+
+	/** Start a new test upon click */
+	public void showPracticeExamView(View view)
+	{
+
+		// reset the selected skills(for the case when user hits back button)
+		this.selectedCategories = new ArrayList<String>();
+
+		// at least
+		boolean atLeastOneSelected = false;
+
+		// get the list of skill levels
+		ListView skillList = (ListView) findViewById(R.id.testCategoryListView);
+
+		// get the indices of the selected items
+		SparseBooleanArray checked = skillList.getCheckedItemPositions();
+
+		int numItemsSelected = skillList.getAdapter().getCount();
+
+
+		for(int i = 0; i < numItemsSelected; i++)
+		{
+			if(checked.get(i))
+			{
+
+				// add all skill levels that were selected by the user
+				selectedCategories.add(skillList.getItemAtPosition(i).toString());
+
+				atLeastOneSelected = true;
+
+			}
+		}
+
+		// make sure user selects at least one item is selected
+		if(!atLeastOneSelected)
+		{
+			Toast.makeText(this, "You must select at least one test category.", Toast.LENGTH_SHORT).show();
+
+			return;
+		}
+
+
+		Intent myIntent = new Intent(CategoryTestMenuActivity.this, PracticeExamActivity.class);
+
+		// save the selected categories levels choosen by the user
+		myIntent.putStringArrayListExtra("selectedCategories", selectedCategories);
+
+		CategoryTestMenuActivity.this.startActivity(myIntent);
 	}
 
 }
--- app/src/main/java/com/mystatscloud/onpoint/SkilledTestMenuActivity.java
+++ app/src/main/java/com/mystatscloud/onpoint/SkilledTestMenuActivity.java
@@ -3,9 +3,11 @@ package com.mystatscloud.onpoint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,8 +23,6 @@ public class SkilledTestMenuActivity extends ActionBarActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-		// add all skill level 3 test questions for debugging
-		selectedSkills.add("3");
 
 		super.onCreate(savedInstanceState);
 
@@ -43,7 +43,7 @@ public class SkilledTestMenuActivity extends ActionBarActivity
 		// use a list with a text view and checkbox layout
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, skillLevel);
 
-		// fill the list with list items
+		// fill the list with list items(skill levels)
 		listView.setAdapter(adapter);
 
 	}
@@ -52,6 +52,44 @@ public class SkilledTestMenuActivity extends ActionBarActivity
 	/** Start a new test upon click */
 	public void showPracticeExamView(View view)
 	{
+
+		// reset the selected skills(for the case when user hits back button)
+		this.selectedSkills = new ArrayList<String>();
+
+		// at least
+		boolean atLeastOneSelected = false;
+
+		// get the list of skill levels
+		ListView skillList = (ListView) findViewById(R.id.testSkillListView);
+
+		// get the indices of the selected items
+		SparseBooleanArray checked = skillList.getCheckedItemPositions();
+
+		int numItemsSelected = skillList.getAdapter().getCount();
+
+
+		for(int i = 0; i < numItemsSelected; i++)
+		{
+			if(checked.get(i))
+			{
+
+				// add all skill levels that were selected by the user
+				selectedSkills.add(skillList.getItemAtPosition(i).toString());
+
+
+				atLeastOneSelected = true;
+
+			}
+		}
+
+		// make sure user selects atleast one item
+		if(!atLeastOneSelected)
+		{
+			Toast.makeText(this, "You must select at least one skill level.", Toast.LENGTH_SHORT).show();
+
+			return;
+		}
+
 
 		Intent myIntent = new Intent(SkilledTestMenuActivity.this, PracticeExamActivity.class);
 
