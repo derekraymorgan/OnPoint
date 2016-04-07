944ea5b52f413adbb9f5a39bf35f07b4bb2d80cc
Ricky Yu <rickyyu2294@gmail.com>
2016-04-07 13:58:05 -0700

    Example Testimonial

--- app/src/main/java/com/mystatscloud/onpoint/TestimonialsActivity.java
+++ app/src/main/java/com/mystatscloud/onpoint/TestimonialsActivity.java
@@ -16,10 +16,11 @@ public class TestimonialsActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState); // make sure onCreate is called for parent class
 
         // temporary place holder testimonial
-        String tempDescription = "This is a temporary testimonial about the On Point app.";
-        String tempAuthor = "-Group 17";
-        String tempDate = "12/1/15";
-        int tempImgResource = R.drawable.gold;
+        String tempDescription = "\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do " +
+                "eiusmod tempor incididunt ut labore et dolore magna aliqua.\"";
+        String tempAuthor = "- Bob Dylan";
+        String tempDate = "4/7/16";
+        int tempImgResource = R.drawable.face;
 
         setContentView(R.layout.activity_testimonials);  // set the xml file being used to draw the activity
 
@@ -30,7 +31,7 @@ public class TestimonialsActivity extends ActionBarActivity {
 
         // populate the data adapter with the testimonials
         int i;
-        int testimonialCount = 10;
+        int testimonialCount = 1;
         for( i = 0; i < testimonialCount; i++){
 
             TestimonialClass obj = new TestimonialClass(tempDescription,tempAuthor,tempDate,tempImgResource);
