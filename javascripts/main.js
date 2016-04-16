b7bf9e1e0296e7fa4a15ab6fc381a6d39e0c5628
derekraymorgan <derek.ray.morgan@gmail.com>
2016-04-16 14:04:27 -0700

    Added a navigation bar, a screen shot slider, images to header.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+	$('.homeSlider').slick({
+
+		dots: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		variableWidth: true,
+		centerMode: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		pauseOnHover: false,
+		centerPadding: '100px'
+	});
+});
