08234de8c38e91281de8050bb50352fe0a325b9d
Ricky Yu <rickyyu2294@gmail.com>
2016-04-22 03:26:28 -0700

    Splash/Menu background

---
+++ app/src/main/java/com/mystatscloud/onpoint/TestFacilityLocator/TestFacilityDetailActivity.java
@@ -0,0 +1,9 @@
+package com.mystatscloud.onpoint.TestFacilityLocator;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Ricky Yu on 4/22/2016.
+ */
+public class TestFacilityDetailActivity extends FragmentActivity {
+}
