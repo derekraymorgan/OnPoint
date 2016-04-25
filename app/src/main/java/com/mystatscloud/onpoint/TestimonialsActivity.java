d831cdc0ca63ed24d7af0be2e1bacb480cfc71cb
Ricky Yu <rickyyu2294@gmail.com>
2016-04-25 16:19:09 -0700

    Additional Testimonials

--- app/src/main/java/com/mystatscloud/onpoint/TestimonialsActivity.java
+++ app/src/main/java/com/mystatscloud/onpoint/TestimonialsActivity.java
@@ -17,13 +17,27 @@ public class TestimonialsActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState); // make sure onCreate is called for parent class
 
         // temporary place holder testimonial
-        String tempDescription = "\"We no longer have to deal with stacks of applications. Now we have a much\n" +
-                "better idea of what we’re getting, and our existing employees know that the\n" +
-                "new team member will be equipped to learn quickly and contribute right away.\n" +
+        String tempDescription = "\"We no longer have to deal with stacks of applications. Now we have a much " +
+                "better idea of what we’re getting, and our existing employees know that the " +
+                "new team member will be equipped to learn quickly and contribute right away. " +
                 "With this program, we’re able to place the right person in the right job.\"";
-        String tempAuthor = "- Leon Osborne, chief executive officer, Osborne Wood Products";
+        String tempAuthor = "- Leon Osborne, Chief Executive Officer, Osborne Wood Products";
         String tempDate = "";
+
+        String tempDescription2 = "\"Taking the assessment validated my skills and showed me areas where " +
+                "I could improve or even add additional skills. The certificate showed " +
+                "potential employers that I would be a valuable part of their workforce " +
+                "with the skills needed to do a good job.\"";
+        String tempAuthor2 = "- ACT NCRC recipient, Georgia";
+
+        String tempDescription3 = "\"Employers work to make things predictable, uniform, and certain. " +
+                "Predictability,uniformity, and certainty are universal goals of executives. The National " +
+                "Career Readiness Certificate does this for the hiring process.\"";
+        String tempAuthor3 = "-Mac Macilroy, Past President, Michigan Manufacturers Association";
+
         int tempImgResource = R.drawable.face;
+        int tempImgResource2 = R.drawable.face2;
+        int tempImgResource3 = R.drawable.face3;
 
         setContentView(R.layout.activity_testimonials);  // set the xml file being used to draw the activity
 
@@ -38,7 +52,11 @@ public class TestimonialsActivity extends ActionBarActivity {
         for( i = 0; i < testimonialCount; i++){
 
             TestimonialClass obj = new TestimonialClass(tempDescription,tempAuthor,tempDate,tempImgResource);
+            TestimonialClass obj2 = new TestimonialClass(tempDescription2, tempAuthor2, tempDate, tempImgResource2);
+            TestimonialClass obj3 = new TestimonialClass(tempDescription3, tempAuthor3, tempDate, tempImgResource3);
             adapter.add(obj);
+            adapter.add(obj2);
+            adapter.add(obj3);
 
         }
 
